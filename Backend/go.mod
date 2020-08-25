2b48eba554652bfa1ab1e1f59200b1783e856b26
David Bourke <davidrbourke@gmail.com>
2020-08-25 21:19:36 +0100

    Intial upload image to folder

---
+++ Backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/davidrbourke/ImageUploader
+
+go 1.13
---
+++ Backend/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("File upload endpoint hit")
+	r.ParseMultipartForm(10 << 20)
+	file, handler, err := r.FormFile("myFile")
+	if err != nil {
+		fmt.Println("Error retrieving the file")
+		fmt.Println(err)
+		return
+	}
+
+	defer file.Close()
+	fmt.Printf("Uploaded file: %+v\n", handler.Filename)
+	fmt.Printf("File size: %+v\n", handler.Size)
+	fmt.Printf("MIME header: %+v\n", handler.Header)
+
+	tempFile, err := ioutil.TempFile("temp-images", "upload-*.png")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer tempFile.Close()
+
+	fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	tempFile.Write(fileBytes)
+
+	fmt.Fprintf(w, "Successfully uploaded file\n")
+}
+
+func setupRoutes() {
+	http.HandleFunc("/upload", uploadFile)
+	http.ListenAndServe(":8080", nil)
+}
+
+func main() {
+	fmt.Println("Starting file uploader")
+	setupRoutes()
+}
